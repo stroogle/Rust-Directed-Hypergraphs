0c9a9c62f35a9e7c4992bd6d353d323a0d268eb5
Colm Moore <colm.moore3@gmail.com>
2025-06-01 12:18:35 +1000

    added bidirectionalhashmap implementation to exports

--- src/directed_hypergraphs/mod.rs
+++ src/directed_hypergraphs/mod.rs
@@ -8,3 +8,4 @@ pub use laplacian::LaplacianDirectedHypergraph;
 pub use bipartite::DirectedBipartiteGraph;
 pub use descriptive::DescriptiveDirectedHypergraph;
 pub use bfgraph::BFDirectedHypergraph;
+pub use bidirectionalhashmap::BidirectionalHashMap;
